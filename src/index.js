a156e95110b0c9e2c9e2e50191477dc7c2fe7b5d
raphaeldefalcoayres <raphaeldefalcoayres@gmail.com>
2020-02-19 23:22:33 -0300

    add configs email and example send mail test ok

--- src/index.js
+++ src/index.js
@@ -4,39 +4,31 @@ const nodemailer = require('nodemailer');
 const cors = require('cors');
 const mailConfig = require('./config/mail');
 
-const transport = {
+const transporter = nodemailer.createTransport({
+  service: 'Zoho',
   host: mailConfig.host,
   port: mailConfig.port,
+  secure: true,
   auth: {
     user: mailConfig.auth.user,
     pass: mailConfig.auth.pass,
   },
-};
-
-const transporter = nodemailer.createTransport(transport);
-
-transporter.verify((error, success) => {
-  if (error) {
-    console.log('error', error);
-  } else {
-    console.log('Server is ready to take messages');
-  }
 });
 
 router.post('/send', (req, res, next) => {
   const name = req.body.name;
   const email = req.body.email;
   const message = req.body.message;
-  const content = `name: ${name} \n email: ${email} \n message: ${message} `;
+  const content = `<b>Nome: </b>${name} <br /><b>E-mail: </b>${email} <br /><b>Mensagem: </b>${message} `;
 
-  const mail = {
-    from: name,
-    to: 'raphaeldefalcoayres@gmail.com', // Change to email address that you want to receive messages on
-    subject: 'New Message from Contact Form',
-    text: content,
+  const mailOptions = {
+    from: '"everton" <everton@spedreal.com.br>',
+    to: 'raphaeldefalcoayres@gmail.com',
+    subject: 'Contato via Site',
+    html: content,
   };
 
-  transporter.sendMail(mail, (err, data) => {
+  transporter.sendMail(mailOptions, (err, data) => {
     if (err) {
       res.json({
         status: 'fail',
